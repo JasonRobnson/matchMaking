2c191a8776cef835672ba25942ecb828698f2eaf
unknown <bubbastinx@gmail.com>
2018-05-10 11:11:08 -0600

    created route to landing page on server

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+// Routes for Express
 app.listen(PORT, function() {
-  console.log(`App listening on port:${  PORT}`);
+  console.log(`App listening on port:${PORT}`);
 });
 
+app.get('/', function(req, res) {
+  res.send('Welcome to the HomePage of this server!');
+});
