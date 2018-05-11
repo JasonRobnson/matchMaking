521ec961eaceadc69595b236882b2b99dcd53072
unknown <bubbastinx@gmail.com>
2018-05-11 10:38:33 -0600

    wonky port promblems no longer exist, created friend.js for existing friends data

--- App/Data/friends.js/friends.js
+++ App/Data/friends.js/friends.js
@@ -1,6 +1,7 @@
 
-let existingFriendsData = [{
+let existingFriendsData = [
     {
+
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
