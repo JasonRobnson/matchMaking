8d7666947e1a69c4270d69bd5d9b18c1c5411260
unknown <bubbastinx@gmail.com>
2018-05-12 10:02:36 -0600

    adjusted routes, and fixed mmy package-json

--- App/routing/htmlRoutes.js
+++ App/routing/htmlRoutes.js
@@ -3,12 +3,13 @@
 module.exports = function(app, path){
 
 
-app.get("/survey.html:surveyResults?", (req, res) => {
+app.get("/survey", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/survey.html"));
+
   console.log("Some one has hit the survey page!")
 })
 
-app.get("/:home?",(req, res) => {
+app.get("/",(req, res) => {
   res.sendFile(path.join(__dirname, "../public/home.html"));
   console.log("Sone one has hit the homepage!")
 });
