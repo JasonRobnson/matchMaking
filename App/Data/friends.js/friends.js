8aabfdf38e80a7b5af8db7f8d69522e1410b21a8
unknown <bubbastinx@gmail.com>
2018-05-13 09:06:32 -0600

    added more friends into the existing friends array,

--- App/Data/friends.js/friends.js
+++ App/Data/friends.js/friends.js
@@ -5,9 +5,43 @@ let existingFriendsData = [
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
+            2,
+            3,
+            6,
+            6,
+            3,
+            3,
+            5,
+            4,
+            4,
+            1
+          ]
+      },
+      {
+
+        "name":"Sarah",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            11,
+            2,
+            3,
+            4,
+            3,
+            2,
+            1,
+            2,
+            4,
+            2
+          ]
+      },
+      {
+
+        "name":"Charlie",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
             5,
             1,
-            4,
+            1,
             4,
             5,
             1,
@@ -16,7 +50,42 @@ let existingFriendsData = [
             4,
             1
           ]
+      },
+      {
+
+        "name":"Kathy",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            11,
+            2,
+            4,
+            3,
+            1,
+            2,
+            2,
+            3,
+            2,
+            1
+          ]
+      },
+      {
+
+        "name":"Sam",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            3,
+            6,
+            3,
+            2,
+            2,
+            3,
+            2,
+            4
+          ]
       }
+      
 ]
 
 
